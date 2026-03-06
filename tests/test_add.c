d3a5a4bd1b28129e4200d6c39c7849519b0fd05c
Victor Hugo Jimenez <victor.jimenez@contractor.brightinsight.com>
2026-03-06 14:11:33 -0500

    parallel approach

--- tests/test_add.c
+++ tests/test_add.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
 #include <assert.h>
 #include "math_utils.h"
 
 int main(int argc, char *argv[]) {
     assert(add(1, 2) == 3);
-    assert(subtract(1, 2) == -1);
     assert(add(0, 0) == 0);
     assert(add(-1, 1) == 0);
-    assert(subtract(10, 3) == 7);
+    assert(add(100, 200) == 300);
     return 0;
 }
--- tests/test_subtract.c
+++ tests/test_subtract.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
 #include <assert.h>
 #include "math_utils.h"
 
 int main(int argc, char *argv[]) {
-    assert(add(1, 2) == 3);
     assert(subtract(1, 2) == -1);
-    assert(add(0, 0) == 0);
-    assert(add(-1, 1) == 0);
     assert(subtract(10, 3) == 7);
+    assert(subtract(0, 5) == -5);
+    assert(subtract(100, 50) == 50);
     return 0;
 }
